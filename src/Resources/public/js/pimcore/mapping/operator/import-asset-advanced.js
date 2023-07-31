4ee3004a59bbed3250406c59d01b28e0e541e9ec
Jeremy Noonan <jeremy.noonan@torqit.ca>
2023-07-31 13:57:21 -0300

    store original URL on asset

--- src/Resources/public/js/pimcore/mapping/operator/import-asset-advanced.js
+++ src/Resources/public/js/pimcore/mapping/operator/import-asset-advanced.js
@@ -25,6 +25,11 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.mapping.operat
             value: this.data.settings ? this.data.settings.path : '/',
         });
 
+        var urlProperty = Ext.create('Ext.form.TextField', {
+            name: 'settings.urlPropertyName',
+            value: this.data.urlPropertyName ? this.data.settings.urlPropertyName : '',
+        });
+
         let pathContainer = Ext.create('Ext.form.FieldContainer', {
             fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_transformation_pipeline_path'),
             layout: 'hbox',
@@ -39,7 +44,21 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.mapping.operat
             }
         });
 
-        formItems.splice(0, 1, pathContainer);
+        let urlContainer = Ext.create('Ext.form.FieldContainer', {
+            fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_transformation_pipeline_url_property'),
+            layout: 'hbox',
+            items: [
+                urlProperty
+            ],
+            width: 900,
+            componentCls: "object_field object_field_type_manyToOneRelation",
+            border: false,
+            style: {
+                padding: 0
+            }
+        });
+
+        formItems.splice(0, 1, pathContainer, urlContainer);
 
         return formItems;
     }
