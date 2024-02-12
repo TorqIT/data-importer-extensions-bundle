7efa59d144a1b28e5b38ff510a9f16a4ec8e7a45
Cameron Jenkins <cameron.jenkins@torqit.ca>
2024-02-12 14:02:37 -0400

    fix class constructor

--- src/Resources/public/js/pimcore/interpreter/optional-elements-xml.js
+++ src/Resources/public/js/pimcore/interpreter/optional-elements-xml.js
@@ -2,8 +2,9 @@ pimcore.registerNS(
   "pimcore.plugin.pimcoreDataImporterBundle.configuration.components.interpreter.optionalElementsXml"
 );
 pimcore.plugin.pimcoreDataImporterBundle.configuration.components.interpreter.optionalElementsXml =
-  new Class(
-    pimcore.plugin.pimcoreDataImporterBundle.configuration.components.interpreter.xml,
+  Class.create(
+    pimcore.plugin.pimcoreDataImporterBundle.configuration.components
+      .interpreter.xml,
     {
       type: "optionElementsXml",
     }
