1cdc09464652cfa38fc67cfcc11685d7b25f207a
Riley Jackson <riley.jackson@torqit.ca>
2024-03-01 12:28:54 -0400

    Fixed newline stuff

--- src/DataSource/Interpreter/BulkCsvFileInterpreter.php
+++ src/DataSource/Interpreter/BulkCsvFileInterpreter.php
@@ -3,31 +3,53 @@
 namespace TorqIT\DataImporterExtensionsBundle\DataSource\Interpreter;
 
 use Carbon\Carbon;
+use Doctrine\DBAL\Exception;
 use Pimcore\Bundle\DataImporterBundle\DataSource\Interpreter\CsvFileInterpreter;
 use Pimcore\Bundle\DataImporterBundle\Processing\ImportProcessingService;
 use Pimcore\Db;
 
 class BulkCsvFileInterpreter extends CsvFileInterpreter
 {
+    /**
+     * @throws Exception
+     */
     protected function doInterpretFileAndCallProcessRow(string $path): void
     {
         $db = Db::get();
         $carbonNow = Carbon::now();
         $timestamp = (int)($carbonNow->getTimestamp() . str_pad((string)$carbonNow->milli, 3, '0'));
         $tmpCsv = tempnam(sys_get_temp_dir(), 'pimcore_bulk_load');
+        $writeDelimiter = $this->delimiter === ';' ? ',' : ';';
+        $writeEnclosure = "'";
+        $writeEscape = '';
 
         if (($readHandle = fopen($path, 'r')) !== false && ($writeHandle = fopen($tmpCsv, 'w')) !== false) {
-            while (($row = fgetcsv($readHandle)) !== false) {
-                if ($this->skipFirstRow) {
+            $currentRow = 0;
+            while (($row = fgetcsv(
+                $readHandle,
+                separator: $this->delimiter,
+                enclosure: $this->enclosure,
+                escape: $this->escape
+                )) !== false) {
+                $currentRow++;
+                if ($currentRow === 1 && $this->skipFirstRow) {
                     continue;
                 }
-                fputcsv($writeHandle, [
-                    $timestamp,
-                    $this->configName,
-                    json_encode($row),
-                    $this->executionType,
-                    ImportProcessingService::JOB_TYPE_PROCESS
-                ]);
+                fputcsv(
+                    $writeHandle,
+                    [
+                        $timestamp,
+                        $this->configName,
+                        json_encode($row),
+                        $this->executionType,
+                        ImportProcessingService::JOB_TYPE_PROCESS
+                    ],
+                    separator: $writeDelimiter,
+                    enclosure: $writeEnclosure,
+                    escape: $writeEscape
+                );
+                fwrite($writeHandle, "\n");
+
             }
             fclose($writeHandle);
             fclose($readHandle);
@@ -37,14 +59,15 @@ class BulkCsvFileInterpreter extends CsvFileInterpreter
         $sql = <<<SQL
         LOAD DATA LOCAL INFILE $quote$tmpCsv$quote INTO TABLE bundle_data_hub_data_importer_queue
             FIELDS 
-                TERMINATED BY ','
-                ENCLOSED BY "'"
-                ESCAPED BY ''
+                TERMINATED BY '$writeDelimiter'
+                ENCLOSED BY "$writeEnclosure"
+                ESCAPED BY '$writeEscape'
             LINES TERMINATED BY '\n'
 
             (timestamp, configName, data, executionType, jobType)
         SQL;
 
         $db->executeQuery($sql);
+        unlink($tmpCsv);
     }
 }
