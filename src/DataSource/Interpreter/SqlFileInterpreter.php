6852826e42770824dba6e412f60f7c8c91f95257
Cameron Jenkins <cameron.jenkins@torqit.ca>
2024-03-13 08:31:24 -0300

    updates according to pimcore suggestions

---
+++ src/DataSource/Interpreter/SqlFileInterpreter.php
@@ -0,0 +1,8 @@
+<?php
+namespace TorqIT\DataImporterExtensionsBundle\DataSource\Interpreter;
+
+use TorqIT\DataImporterExtensionsBundle\DataSource\Interpreter\BulkCsvFileInterpreter;
+
+class SqlFileInterpreter extends BulkCsvFileInterpreter{
+    
+}
--- src/Resources/public/js/pimcore/loader/sql.js
+++ src/Resources/public/js/pimcore/loader/sql.js
@@ -1,16 +1,3 @@
-/**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
- */
-
 pimcore.registerNS("pimcore.plugin.pimcoreDataImporterBundle.configuration.components.loader.sql");
 pimcore.plugin.pimcoreDataImporterBundle.configuration.components.loader.sql = Class.create(pimcore.plugin.pimcoreDataImporterBundle.configuration.components.abstractOptionType, {
 
@@ -18,12 +5,12 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.loader.sql = C
 
     buildSettingsForm: function() {
 
-        if (!this.form) {
+        if(!this.form) {
             const dataStore = Ext.create('Ext.data.Store', {
                 autoLoad: true,
                 proxy: {
                     type: 'ajax',
-                    url: '/admin/pimcoredataimporter/get-connections',
+                    url: Routing.generate('pimcore_dataimporter_connections'),
                     reader: {
                         type: 'json'
                     }
@@ -39,28 +26,66 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.loader.sql = C
                 items: [
                     {
                         xtype: 'combobox',
-                        name: this.dataNamePrefix + 'connectionName',
-                        value: this.data.connectionName,
-                        fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_loader_connection_name'),
+                        fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_sql_connection'),
+                        name: this.dataNamePrefix + 'connection',
+                        value: this.data.connection,
+                        allowBlank: false,
+                        msgTarget: 'under',
                         displayField: 'name',
                         valueField: 'value',
                         store: dataStore,
                     },
                     {
-                        xtype: 'textareafield',
-                        fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_type_sql'),
-                        name: this.dataNamePrefix + 'sql',
-                        value: this.data.sql,
-                        allowBlank: false,
+                        xtype: 'textarea',
+                        fieldLabel: "SELECT <br /><small>(eg. a,b,c)*</small>",
+                        name: this.dataNamePrefix + 'select',
+                        value: this.data.select,
                         msgTarget: 'under',
-                        scrollable: true,
-                        maxRows: 20,
-                        height:500
-                    }
+                        width: 900,
+                        height: 200,
+                        grow: true,
+                        growMax: 400,
+                        allowBlank: false,
+                    },
+                    {
+                        xtype: 'textarea',
+                        fieldLabel: "FROM <br /><small>(eg. d INNER JOIN e ON c.a = e.b)*</small>",
+                        name: this.dataNamePrefix + 'from',
+                        value: this.data.from,
+                        msgTarget: 'under',
+                        width: 900,
+                        height: 200,
+                        grow: true,
+                        growMax: 400,
+                        allowBlank: false,
+                    },
+                    {
+                        xtype: 'textarea',
+                        fieldLabel: "WHERE <br /><small>(eg. c = 'some_value')</small>",
+                        name: this.dataNamePrefix + 'where',
+                        value: this.data.where,
+                        msgTarget: 'under',
+                        width: 900,
+                        height: 200,
+                        grow: true,
+                        growMax: 400,
+                    },
+                    {
+                        xtype: 'textarea',
+                        fieldLabel: "GROUP BY <br /><small>(eg. b, c )</small>",
+                        name: this.dataNamePrefix + 'groupBy',
+                        value: this.data.groupBy,
+                        msgTarget: 'under',
+                        width: 900,
+                        height: 200,
+                        grow: true,
+                        growMax: 400,
+                    },
                 ]
             });
         }
 
         return this.form;
     }
+
 });
