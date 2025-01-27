eda851ceb8bb37b5a818fc417588dc132ecfd9a1
Sean MacKay <seanrmackay@gmail.com>
2025-01-27 17:25:54 -0400

    Revert code accidentally copied from Pimcore

--- src/DataSource/Loader/BulkSqlDataLoader.php
+++ src/DataSource/Loader/BulkSqlDataLoader.php
@@ -2,19 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
- */
-
 namespace TorqIT\DataImporterExtensionsBundle\DataSource\Loader;
 
 use Doctrine\DBAL\Connection;
@@ -25,7 +12,12 @@ use League\Flysystem\Local\LocalFilesystemAdapter;
 use Pimcore;
 use Pimcore\Bundle\DataImporterBundle\DataSource\Loader\DataLoaderInterface;
 use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
+use Pimcore\Logger;
 use Symfony\Component;
+use TorqIT\DataImporterExtensionsBundle\Exception\FetchDatabaseDataException;
+use TorqIT\DataImporterExtensionsBundle\Exception\InvalidConnectionException;
+use TorqIT\DataImporterExtensionsBundle\Exception\NotResourceException;
+use TorqIT\DataImporterExtensionsBundle\Exception\ParseArrayToJsonException;
 
 class BulkSqlDataLoader implements DataLoaderInterface
 {
@@ -34,58 +26,67 @@ class BulkSqlDataLoader implements DataLoaderInterface
     private string $from;
     private string $where;
     private string $groupBy;
+    private ?int $limit;
 
     private string $importFilePath;
     private Connection $databaseConnection;
 
-    public function __construct(private Component\Filesystem\Filesystem $filesystem)
+    public function __construct(protected Component\Filesystem\Filesystem $filesystem)
     {
     }
 
     /**
      * @throws InvalidConfigurationException
-     * @throws Exception
-     * @throws FilesystemException
+     * @throws InvalidConnectionException
+     * @throws FetchDatabaseDataException
+     * @throws NotResourceException
+     * @throws ParseArrayToJsonException
      */
     public function loadData(): string
     {
         $this->setUpConnection();
         $this->setUpImportFilePath();
-
         $queryBuilder = $this->databaseConnection->createQueryBuilder();
         $queryBuilder->select($this->select)
             ->from($this->from);
-
         if (!empty($this->where)) {
             $queryBuilder->where($this->where);
         }
-
         if (!empty($this->groupBy)) {
             $queryBuilder->groupBy($this->groupBy);
         }
 
-        $result = $queryBuilder->execute()->fetchAllAssociative();
+        if($this->limit){
+            $queryBuilder->setMaxResults($this->limit);
+        }
+
+        $results = $queryBuilder->executeQuery();
 
         $filesystemLocal = new Filesystem(new LocalFilesystemAdapter('/'));
         $stream = fopen('php://temp', 'r+');
-        $resultAsJson = json_encode($result);
+        $columnNamesAdded = false;
+        while (($result = $results->fetchAssociative()) !== false) {
+            if (!$columnNamesAdded) {
+                fputcsv($stream, array_keys($result));
+                $columnNamesAdded = true;
+            }
+            fputcsv($stream, $result);
+        }
 
-        fwrite($stream, $resultAsJson);
         rewind($stream);
 
         $filesystemLocal->writeStream($this->importFilePath, $stream);
-
         return $this->importFilePath;
     }
-
     public function cleanup(): void
     {
         $this->databaseConnection->close();
-
         unlink($this->importFilePath);
     }
 
     /**
+     * @param array<string, string> $settings
+     *
      * @throws InvalidConfigurationException
      */
     public function setSettings(array $settings): void
@@ -94,12 +95,10 @@ class BulkSqlDataLoader implements DataLoaderInterface
             throw new InvalidConfigurationException('Empty connection.');
         }
         $this->connection = $settings['connection'];
-
         if (empty($settings['select'])) {
             throw new InvalidConfigurationException('Empty select.');
         }
         $this->select = $settings['select'];
-
         if (empty($settings['from'])) {
             throw new InvalidConfigurationException('Empty from.');
         }
@@ -107,22 +106,23 @@ class BulkSqlDataLoader implements DataLoaderInterface
 
         $this->where = $settings['where'];
         $this->groupBy = $settings['groupBy'];
+
+        $this->limit = array_key_exists('limit', $settings) ? intval($settings['limit']) : null;
     }
 
     /**
-     * @throws InvalidConfigurationException
+     * @throws InvalidConnectionException
      */
     private function setUpConnection(): void
     {
         $container = Pimcore::getContainer();
         $databaseConnection = null;
-
         if ($container instanceof Component\DependencyInjection\ContainerInterface) {
             $databaseConnection = $container->get($this->connection);
         }
 
         if (!$databaseConnection instanceof Connection) {
-            throw new InvalidConfigurationException('Connection not found.');
+            throw new InvalidConnectionException('Connection not found');
         }
 
         $this->databaseConnection = $databaseConnection;
