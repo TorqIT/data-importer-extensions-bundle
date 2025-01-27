c64c6df448e6795e62dd35b0c68e71e170d4b424
Sean MacKay <seanrmackay@gmail.com>
2025-01-27 16:45:13 -0400

    Fix namespace

--- src/DataSource/Loader/BulkSqlDataLoader.php
+++ src/DataSource/Loader/BulkSqlDataLoader.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-namespace Pimcore\Bundle\DataImporterBundle\DataSource\Loader;
+namespace TorqIT\DataImporterExtensionsBundle\DataSource\Loader;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception;
