fab469ab38a8d3f9e765d4c076cd953ec5b566a5
Sean MacKay <seanrmackay@gmail.com>
2025-01-27 17:13:43 -0400

    Remove extra BulkCSV interpreter settings

--- src/Resources/public/js/pimcore/interpreter/bulk-csv.js
+++ src/Resources/public/js/pimcore/interpreter/bulk-csv.js
@@ -32,25 +32,7 @@ pimcore.plugin.pimcoreDataImporterBundle.configuration.components.interpreter.bu
                         name: this.dataNamePrefix + 'skipFirstRow',
                         value: this.data.hasOwnProperty('skipFirstRow') ? this.data.skipFirstRow : false,
                         inputValue: true
-                    },{
-                        xtype: 'textfield',
-                        fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_csv_delimiter'),
-                        name: this.dataNamePrefix + 'delimiter',
-                        value: this.data.delimiter || ',',
-                        width: 250
-                    },{
-                        xtype: 'textfield',
-                        fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_csv_enclosure'),
-                        name: this.dataNamePrefix + 'enclosure',
-                        value: this.data.enclosure || '"',
-                        width: 250
-                    },{
-                        xtype: 'textfield',
-                        fieldLabel: t('plugin_pimcore_datahub_data_importer_configpanel_csv_escape'),
-                        name: this.dataNamePrefix + 'escape',
-                        value: this.data.escape || '\\',
-                        width: 250
-                    },
+                    }
                 ]
             });
         }
