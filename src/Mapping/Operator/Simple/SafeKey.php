ab9921bcd71f05c977a90911380a034306ee4d5b
Riley Jackson <riley.jackson@torqit.ca>
2026-04-14 13:12:07 -0300

    Fix missing tag on SafeKey.php

--- src/Mapping/Operator/Simple/SafeKey.php
+++ src/Mapping/Operator/Simple/SafeKey.php
@@ -6,7 +6,9 @@ namespace TorqIT\DataImporterExtensionsBundle\Mapping\Operator\Simple;
 use Pimcore\Bundle\DataImporterBundle\Mapping\Operator\AbstractOperator;
 use Pimcore\Model\Element\Service as ElementService;
 use Pimcore\Bundle\DataImporterBundle\Exception\InvalidConfigurationException;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
+#[AutoconfigureTag(name: 'pimcore.datahub.data_importer.operator', attributes: ['type' => 'safeKey'])]
 class SafeKey extends AbstractOperator
 {
     public function setSettings(array $settings): void
