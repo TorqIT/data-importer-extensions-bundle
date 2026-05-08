17bc2b2a2c240cc5bb07327c1932f1e813fb8dee
Riley Jackson <riley.jackson@torqit.ca>
2026-05-08 09:32:58 -0300

    remove set logger calls

--- src/DataSource/Interpreter/BulkCsvFileInterpreter.php
+++ src/DataSource/Interpreter/BulkCsvFileInterpreter.php
@@ -6,7 +6,8 @@ use Pimcore\Bundle\DataImporterBundle\DataSource\Interpreter\CsvFileInterpreter;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
-#[Autoconfigure(calls: [['setLogger', ['@logger']]])]
+// FIXME: setLogger currently broken
+//#[Autoconfigure(calls: [['setLogger', ['@logger']]])]
 #[AutoconfigureTag(name: 'monolog.logger', attributes: ['channel' => 'DATA-IMPORTER'])]
 #[AutoconfigureTag(name: 'pimcore.datahub.data_importer.interpreter', attributes: ['type' => 'bulkCsv'])]
 // FIXME: CsvFileInterpreter is now final, cannot extend
